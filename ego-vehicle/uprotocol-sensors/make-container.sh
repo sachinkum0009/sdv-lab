61d2bbadae6d75b47d48258f2261b94679f64ec9
Sachin Kumar <sachinkum123567@gmail.com>
2025-09-30 16:49:51 +0200

    docker image working

--- ego-vehicle/uprotocol-sensors/make-container.sh
+++ ego-vehicle/uprotocol-sensors/make-container.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Works whether you're using sudo or not
-UID=$(id -u "${SUDO_USER:-$USER}")
-GID=$(id -g "${SUDO_USER:-$USER}")
+USER_UID=$(id -u "${SUDO_USER:-$USER}")
+USER_GID=$(id -g "${SUDO_USER:-$USER}")
 
 TARGET=${1:-DEV}
 
 sudo podman build \
   --target $TARGET \
-  --build-arg USER_UID=$UID \
-  --build-arg USER_GID=$GID \
+  --build-arg USER_UID=$USER_UID \
+  --build-arg USER_GID=$USER_GID \
   --build-arg USERNAME=dev \
   -t uprotocol-sensors .
